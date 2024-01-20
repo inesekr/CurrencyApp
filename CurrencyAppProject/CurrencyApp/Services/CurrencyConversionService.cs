d1ed38f9e9af6305dd70d56bfdb844e5d309bd44
Inese Krole <inese.krole@gmail.com>
2024-01-20 21:02:19 +0200

    update

--- CurrencyAppProject/CurrencyApp/Services/CurrencyConversionService.cs
+++ CurrencyAppProject/CurrencyApp/Services/CurrencyConversionService.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.Extensions.Configuration;
-
-namespace CurrencyApp.Services
+﻿namespace CurrencyApp.Services
 {
     public class CurrencyConversionService : ICurrencyConversionService
     {
